2ef36d5933337763a7a2f69ea1feef4293f5b464
Todd Mohney <toddmohney@gmail.com>
2016-04-16 11:08:15 -0400

    Adds basic implementation of ResourceObject
    [Resolves #1]
    
    Implements a representation of a ResourceObject
    
    This implementation does not include the relationships, links, or meta
    objects.
    
    Sample structure:
    { "id": 1,
      "type": "SomeType",
      "attributes": {
        "name": "Magnum P.I.",
        "job": "Private Eye"
      }
    }

--- src/JsonApi.hs
+++ src/JsonApi.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module JsonApi
-( gimmeFive
+( ResourceId (..)
+, ResourceObject (..)
+, ResourceType (..)
+, ToResourceObject (..)
 ) where
 
-gimmeFive :: Int
-gimmeFive = 5
+import Control.Monad (mzero)
+import Data.Aeson ((.=), (.:))
+import qualified Data.Aeson as AE
+import Data.Text (Text)
+
+class (AE.ToJSON a) => ToResourceObject a where
+  toResource :: a -> ResourceObject a
+
+newtype ResourceId = ResourceId Text
+  deriving (Show, Eq, Ord, AE.ToJSON, AE.FromJSON)
+
+newtype ResourceType = ResourceType Text
+  deriving (Show, Eq, Ord, AE.ToJSON, AE.FromJSON)
+
+data ResourceObject a = ResourceObject ResourceId ResourceType a
+  deriving (Show, Eq, Ord)
+
+instance (AE.ToJSON a) => AE.ToJSON (ResourceObject a) where
+  toJSON (ResourceObject resId resType resObj) =
+    AE.object [ "id"         .= resId
+              , "type"       .= resType
+              , "attributes" .= resObj
+              ]
+
+instance (AE.FromJSON a) => AE.FromJSON (ResourceObject a) where
+  parseJSON (AE.Object v) = ResourceObject <$>
+                              v .: "id" <*>
+                              v .: "type" <*>
+                              v .: "attributes"
+  parseJSON _          = mzero
+
--- test/JsonApiSpec.hs
+++ test/JsonApiSpec.hs
@@ -1,14 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module JsonApiSpec where
 
+import qualified Data.Aeson as AE
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.Maybe (isJust, fromJust)
+import Data.Text (Text, pack)
+import GHC.Generics
 import JsonApi
 import Test.Hspec
+
+data TestObject =
+  TestObject { myId :: Int
+             , myName :: Text
+             , myAge :: Int
+             , myFavoriteFood :: Text
+             } deriving (Show, Generic)
+
+instance AE.ToJSON TestObject
+instance AE.FromJSON TestObject
+
+instance ToResourceObject TestObject where
+  toResource a =
+    ResourceObject
+      (ResourceId . pack . show . myId $ a)
+      (ResourceType "TestObject")
+      a
+
+testObject :: TestObject
+testObject = TestObject 1 "Fred Armisen" 49 "Pizza"
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "gimmeFive" $ do
-    it "returns 5" $ do
-      gimmeFive `shouldBe` 5
+  describe "ToResourceObject" $ do
+    it "can be encoded and decoded from JSON" $ do
+      let encodedJson = BS.unpack . AE.encode . toResource $ testObject
+      putStrLn encodedJson
+      let decodedJson = AE.decode (BS.pack encodedJson) :: Maybe (ResourceObject TestObject)
+      putStrLn $ show . fromJust $ decodedJson
+      (isJust decodedJson) `shouldBe` True
+
