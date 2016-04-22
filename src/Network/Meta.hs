84398f451014db1408e94a9c25f0b877875448e1
Todd Mohney <toddmohney@gmail.com>
2016-04-22 14:40:53 +0000

    Add Meta ADT

---
+++ src/Network/Meta.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Network.Meta where
+
+import           Data.Aeson (ToJSON, FromJSON)
+import           Data.Map (Map)
+import           Data.Text (Text)
+import           GHC.Generics
+
+data Meta a = Meta (Map Text a)
+  deriving (Show, Eq, Ord, Generic)
+
+instance ToJSON a   => ToJSON (Meta a)
+instance FromJSON a => FromJSON (Meta a)
+
---
+++ test/Network/MetaSpec.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Network.MetaSpec where
+
+import qualified Data.Aeson as AE
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Map as Map
+import           Data.Maybe (isJust)
+import           Network.Meta
+import           TestHelpers (prettyEncode)
+import           Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "serialization" $ do
+    -- is there a compelling reason to test this?
+    --
+    it "serializes/deserializes primitive values" $ do
+      let intTestData = Meta . Map.fromList $ [ ("numData", (5 :: Int)) ]
+      let encIntJson = BS.unpack . prettyEncode $ intTestData
+      let decIntJson = AE.decode (BS.pack encIntJson) :: Maybe (Meta Int)
+      (isJust decIntJson) `shouldBe` True
+      -- putStrLn (keys encIntJson)
+      -- putStrLn $ show . fromJust $ decIntJson
+
+      let boolTestData = Meta . Map.fromList $ [ ("boolData", True) ]
+      let encBoolJson = BS.unpack . prettyEncode $ boolTestData
+      let decBoolJson = AE.decode (BS.pack encBoolJson) :: Maybe (Meta Bool)
+      (isJust decBoolJson) `shouldBe` True
+      -- putStrLn (keys encBoolJson)
+      -- putStrLn $ show . fromJust $ decBoolJson
+
