591226dbdda6f710c40aec272aab91296c1c6c26
Todd Mohney <toddmohney@gmail.com>
2016-07-05 11:14:09 -0400

    Remove Error data constructor from Document
    - This is a temporary measure so we can merge into master
      without introducing have built solutions
    
    - There is remaining problem of a mismatch of constraint variables
      in the Document's To/FromJSON instances keeping us from completing the
      addition of the Error data constructor to Document

--- src/Network/JSONApi/Document.hs
+++ src/Network/JSONApi/Document.hs
@@ -10,15 +10,12 @@ module Network.JSONApi.Document
 
 import           Data.Aeson (ToJSON, FromJSON, (.=), (.:), (.:?))
 import qualified Data.Aeson as AE
-import qualified Data.HashMap.Strict as HM
-import qualified Network.JSONApi.Document.Error as Doc
 import qualified Network.JSONApi.Document.Success as Doc
 import           Network.JSONApi.Link as L
 import           Network.JSONApi.Meta as M
 import qualified Network.JSONApi.ResourceObject as RO
 
 data Document a b c = Resource (Doc.Success a b c)
-                    | Error (Doc.Error a b)
                    deriving (Show, Eq)
 
 instance (ToJSON a, ToJSON b, ToJSON c) => ToJSON (Document a b c) where
@@ -27,22 +24,10 @@ instance (ToJSON a, ToJSON b, ToJSON c) => ToJSON (Document a b c) where
               , "links" .= links
               , "meta"  .= meta
               ]
-  toJSON (Error (Doc.Error err links meta)) =
-    AE.object [ "error" .= err
-              , "links" .= links
-              , "meta"  .= meta
-              ]
 
 instance (FromJSON a, FromJSON b, FromJSON c) => FromJSON (Document a b c) where
-  parseJSON = AE.withObject "document" $ \v ->
-    case HM.lookup "error" v of
-      Nothing -> do
-        d <- v .:  "data"
-        l <- v .:? "links"
-        m <- v .:? "meta"
-        return (Resource $ Doc.Success d l m)
-      (Just _) -> do
-        e <- v .:  "error"
-        l <- v .:? "links"
-        m <- v .:? "meta"
-        return (Error $ Doc.Error e l m)
+  parseJSON = AE.withObject "document" $ \v -> do
+    d <- v .:  "data"
+    l <- v .:? "links"
+    m <- v .:? "meta"
+    return (Resource $ Doc.Success d l m)
